c0dad0ae2a46b17c828769119026e09ebafb4f50
suji3kr <cutesuji3kr@naver.com>
2024-11-20 15:44:08 +0900

    클래스 다이어 그램_ UML

---
+++ java_code1/Model/Smart Phone.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Smart Phone {
+
+    public Smart Phone() {
+    }
+
+    private void serialNumber String;
+
+    private void manufacturer String;
+
+    private String agency;
+
+    public void Attribute1;
+
+    public void amartPhone() {
+
+    }
+
+    public String usingPeopleinfo() {
+
+        return "";
+    }
+
+}
---
+++ java_code2/HelloWord.java
@@ -0,0 +1,9 @@
+import java.awt.Graphics;
+import javax.swing.jApplet;
+
+public class heloWorld extends JApplet{
+	public void paoint (Graphics g) {
+
+		g.drawString("Hello, Word!", 10, 10);
+	}
+}
